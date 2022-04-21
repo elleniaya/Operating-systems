af30a380bac898b7f0c9269ca85c1ca2a838b1b1
Елена Евмененко <98080553+elleniaya@users.noreply.github.com>
2022-04-21 23:54:44 +0700

    Update lab9_2.c

--- lab9/lab9_2.c
+++ lab9/lab9_2.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
+#define FORK_ERROR -1
+#define ERROR 1
+#define CHILD 0
+#define EXECLP_ERROR -1
+#define WAIT_ERROR -1
+
+int main() {
+  pid_t pid = fork();
+  if (pid == FORK_ERROR) {
+        perror("fork error");
+        return ERROR;
+  }
+  
+  if (pid == CHILD) {
+        execlp("cat", "cat", "text.txt", NULL);
+        perror("execlp error");
+        return EXECLP_ERROR;
+  }
+  
+  pid_t wait_res = wait(NULL);
+  if (wait_res == WAIT_ERROR) {
+        perror("wait error");
+        return WAIT_ERROR;
+  }
+  
+  printf("This is the parent process\n");
+  return 0;
+}
