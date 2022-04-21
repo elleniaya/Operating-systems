433c355d37344390dbd01b8b593a973aa3533703
Елена Евмененко <98080553+elleniaya@users.noreply.github.com>
2022-04-21 23:47:03 +0700

    Update lab9_1.c

--- lab9/lab9_1.c
+++ lab9/lab9_1.c
@@ -1,1 +1,23 @@
-//
+#include <stdio.h>
+#include <unistd.h>
+
+#define FORK_ERROR -1
+#define ERROR 1
+#define CHILD 0
+
+int main() {
+  pid_t pid = fork();
+  if (pid == FORK_ERROR) {
+        perror("fork error");
+        return ERROR;
+  }
+  
+  if (pid == CHILD) {
+        execlp("cat", "cat", "text.txt", NULL);
+        perror("execlp error");
+        return ERROR;
+  }
+  
+  printf("This is the parent process\n");
+  return 0;
+}
