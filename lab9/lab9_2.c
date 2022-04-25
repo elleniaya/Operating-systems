7de6107fa4a4f7b2200a27da77d3307a24f0baae
Елена Евмененко <98080553+elleniaya@users.noreply.github.com>
2022-04-25 15:47:08 +0700

    Update lab9_2.c

--- lab9/lab9_2.c
+++ lab9/lab9_2.c
@@ -8,7 +8,7 @@
 #define EXECLP_ERROR -1
 #define WAIT_ERROR -1
 
-int main() {
+int main(int argc, char** argv) {
   pid_t pid = fork();
   if (pid == FORK_ERROR) {
         perror("fork error");
@@ -16,7 +16,7 @@ int main() {
   }
   
   if (pid == CHILD) {
-        execlp("cat", "cat", "text.txt", NULL);
+        execlp("cat", "cat", argv[1], NULL);
         perror("execlp error");
         return EXECLP_ERROR;
   }
